fa06ded211ce541d3252908d727f888c1f5db44f
Giselli Hara <gi@hara.ninja>
2021-05-24 22:33:18 -0300

    erros corrigidos

--- src/main/java/dev/giselli/tarefa1/entities/Client.java
+++ src/main/java/dev/giselli/tarefa1/entities/Client.java
@@ -2,8 +2,19 @@ package dev.giselli.tarefa1.entities;
 
 import java.time.Instant;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
+
 public class Client {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String cpf;
